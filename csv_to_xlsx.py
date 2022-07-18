839351a6de3c41b659f7892d01b6f735e7e65256
Marcus Diemand <mjdiemand@gmail.com>
2022-07-18 15:40:03 -0400

    added csv_to_xlsx.py

---
+++ csv_to_xlsx.py
@@ -0,0 +1,17 @@
+from tabnanny import filename_only
+import pandas as pd
+import openpyxl
+import os
+
+DATA_FOLDER_PATH = '.\\data\\'
+
+TOP_HASHTAGS_PER_DAY_FOLDER_PATH =  DATA_FOLDER_PATH + "top_hashtags_per_day_utf-8\\"
+TOP_HASHTAGS_XLSX_PER_DAY_FOLDER_PATH =  DATA_FOLDER_PATH + "top_hashtags_per_day_utf-8_xlsx\\"
+
+for filename in os.listdir(TOP_HASHTAGS_PER_DAY_FOLDER_PATH):
+    read_file = pd.read_csv(os.path.join(TOP_HASHTAGS_PER_DAY_FOLDER_PATH, filename))
+    print("converting " + filename + " to xlsx") 
+    
+    filename_without_extension = filename.split('.')[0]
+    filename = filename_without_extension + ".xlsx"
+    read_file.to_excel (os.path.join(TOP_HASHTAGS_XLSX_PER_DAY_FOLDER_PATH, filename), index = None, header=True)
